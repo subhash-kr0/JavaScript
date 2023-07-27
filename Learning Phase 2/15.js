bfe330af657fd6d480f93a80b0bd7b59bd69bf06
subhashkr1 <subhashkr855@gmail.com>
2023-07-27 21:28:13 +0530

    LP-2

---
+++ Learning Phase 2/15.js
@@ -0,0 +1,18 @@
+// ternary operator 
+
+let age = 4;
+let drink;
+
+if(age>=5){
+    drink = "coffee";
+}else{
+    drink = "milk";
+}
+
+console.log(drink);
+
+// ternary operator / conditional operator 
+
+let age1 = 3;
+let drink1 = age >= 5 ? "coffee" : "milk";
+console.log(drink1);
